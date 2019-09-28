fb0dfe867556242f2cb5e13a25b04597b535ec56
Jack Bragg <jack.d.bragg@gmail.com>
2019-09-27 21:00:44 -0700

    updating the search alg

--- FitAFSite/static/FitAFSite/js/index.js
+++ FitAFSite/static/FitAFSite/js/index.js
@@ -33,6 +33,10 @@ class Recipe {
         this.image = ''
         this.calories = 0
         this.cooktime = 0
+        this.yield = 1
+        this.fat = 0.0
+        this.carb = 0.0
+        this.pro = 0.0
     }
 };
 
@@ -163,21 +167,45 @@ const app = new Vue({
             get = this.api + ingr + this.app_id + this.app_key + rCals
             const response = await axios.get(get)
             console.log(response)
+
+            this.recipe.calories = response.data.hits[0].recipe.calories
+            this.recipe.yield = response.data.hits[0].recipe.yield
+            console.log(this.recipe.calories, this.recipe.yield)
             this.recipe.url = response.data.hits[0].recipe.url
             this.recipe.image = response.data.hits[0].recipe.image
-            this.recipe.calories = response.data.hits[0].recipe.calories
             this.recipe.cooktime = response.data.hits[0].recipe.totalNutrients.totalTime
             console.log(this.recipe)
         },
 
         _search_hits: function(hits) {
             // This takes in 10 recipe hits from API and searches for most relevent
-            var img = hits[0].recipe.image
-            var cal = hits[0].recipe.calories
-            var cooktime = hits[0].recipe.totalNutrients.totalTime
-            var fat = hits[0].recipe.digest['Fat']
-            var carb = hits[0].recipe.digest['Carbs']
-            var pro = hits[0].recipe.digest['Protein']
+            var closest = new Recipe()
+            var recipes = []
+            for (i=0; i <= hits.length; i++) {
+                var rcp = new Recipe()
+                this.rcp.url = hits[i].recipe.url
+                this.rcp.image = hits[i].recipe.image
+                this.rcp.calories = hits[i].recipe.calories
+                this.rcp.cooktime = hits[i].recipe.totalNutrients.totalTime
+                this.rcp.yield = hits[i].recipe.yield
+                this.rcp.fat = hits[i].recipe.digest['Fat']
+                this.rcp.carb = hits[i].recipe.digest['Carbs']
+                this.rcp.pro = hits[i].recipe.digest['Protein']
+                this.rcp.serv_cal = Math.floor(this.rcp.calories / this.rcp.yield)
+                this.recipes.push(this.rcp)
+            }
+            this.closest = recipes[0]
+            if (this.recipes.length > 1) {
+                for (i=1; i <= this.recipes.length; i++){
+                    var cl_dist = this.owner.daily_calorie - this.closest.serv_cal
+                    var nxt_dist = this.owner.daily_calorie - this.recipes[i].serv_cal
+                    if (this.cl_dist > this.nxt_dist) {
+                        // if this recipe is closer to the remaining cals then use this
+                        this.closest = this.recipes[i]
+                    }
+                }
+            }
+
         },
 
         // grabs api keys
@@ -195,3 +223,6 @@ const app = new Vue({
         this.getMeal()
     }
 });
+                rcp.carb
+                rcp.pro
+                rcp.serv_cal

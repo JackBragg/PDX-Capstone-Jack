6c392091c29185077219a713bd3298cc543a0498
Jack Bragg <jack.d.bragg@gmail.com>
2019-10-03 20:43:41 -0700

    css all the things

--- FitAFSite/static/FitAFSite/js/index.js
+++ FitAFSite/static/FitAFSite/js/index.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var drop_instances = M.Dropdown.init(elems);
     var elem = document.querySelectorAll('.modal');
     var modal_instance = M.Modal.init(elem);
+    var nav_elems = document.querySelectorAll('.sidenav');
+    var nav_instances = M.Sidenav.init(nav_elems);
   });
     
 
